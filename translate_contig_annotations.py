3e2f31dece2c26af15a8d44984c1ab8e00e58ca8
samfway <samfway@gmail.com>
2014-02-13 13:45:43 -0700

    Broke translation/output file creation into separate functions

--- translate_contig_annotations.py
+++ translate_contig_annotations.py
@@ -41,8 +41,7 @@ def translate_annotation(location, nucmer_record, reference_name=None):
                                        reference_name)
     return new_location 
 
-def translate_annotations(predicted_genes_file, alignment_coords_file, output_file, \
-    reference_name='reference_genome'):
+def get_translated_annotations(predicted_genes_file, alignment_coords_file):
     """ Translate annotation coordinates back to reference sequences. 
         predicted_genes_file should be a GFF file containing annotations 
           for a collection of contigs.
@@ -52,34 +51,39 @@ def translate_annotations(predicted_genes_file, alignment_coords_file, output_fi
         With these two files, this function maps genes/features found in contigs
         back to the reference sequence.  This can be used to compare the number of
         genes found in contigs versus those found in the reference genome 
-
-        reference_name denotes the sequence identifier to be used 
-        in the first field of the resulting gff file. 
     """ 
-    output = open(output_file, 'w')
-    output.write('##gff out for translated genes\n')
-    output.write('##gff-version 3\n')
-    id_count = 0
     nucmer_records = nm.get_nucmer_records_by_contig(alignment_coords_file)
     for gene in pl.get_feature_locations(predicted_genes_file):
         if gene.seq_id in nucmer_records:
             for record in nucmer_records[gene.seq_id]:
                 new_location = translate_annotation(gene, record)
                 if new_location is not None:
-                    fields = [reference_name, \
-                              'translated', \
-                              'gene', \
-                              str(new_location.start), \
-                              str(new_location.end), \
-                              '.', \
-                              ('+' if new_location.strand > 0 else '-'), \
-                              '.', \
-                              'id_%d' % (id_count)] 
-                    output.write('\t'.join(fields)+'\n')
-                    id_count += 1 
+                    yield new_location
+
+def create_translated_file(predicted_genes_file, alignment_coords_file, output_file, \
+    reference_name='reference_genome'):
+    """ Translate annotations and save to output file 
+    """ 
+    output = open(output_file, 'w')
+    output.write('##gff out for translated genes\n')
+    output.write('##gff-version 3\n')
+    id_count = 0
+    for new_location in get_translated_annotations(predicted_genes_file, alignment_coords_file):
+        fields = [reference_name, \
+                  'translated', \
+                  'gene', \
+                  str(new_location.start), \
+                  str(new_location.end), \
+                  '.', \
+                  ('+' if new_location.strand > 0 else '-'), \
+                  '.', \
+                  'id_%d' % (id_count)] 
+        output.write('\t'.join(fields)+'\n')
+        id_count += 1 
     output.close() 
+
 
 if __name__=="__main__":
     args = interface() 
-    translate_annotations(args.predicted_genes, args.alignment_coords, args.output_file)
+    create_translated_file(args.predicted_genes, args.alignment_coords, args.output_file)
 
